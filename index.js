0682544a4c75cfc48258274ff379b0c92c603e49
MarksIT23 <markangelo2304@gmail.com>
2024-03-14 15:57:36 +0800

    updated js

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+
 console.log("Hello World.");
 
-function generateRandomNumber() 
-{
-    let randomNum = Math.floor(Math.random() * 10) + 4;
+generateRandomNumber(30);
 
-if(randomNum >= 5)
+function generateRandomNumber(max) 
 {
-    console.log(`Random Number is >= to 5`);
-} else {
-    console.log(`Random Number is <= to 5`);
+    let randomNum = Math.floor(Math.random() * max) + 1;
+
+if (randomNum <= 15){
+    return console.log(`Random number ${randomNum} is in B. 1 - 15`);
+} else if (randomNum >= 16 && randomNum <= 30) {
+return console.log(`Random number ${randomNum} is in I. 16 - 30`);
+} else if (randomNum >= 31 && randomNum <= 45) {
+    return console.log(`Random number ${randomNum} is in N. 31 - 45`);
+}  else if (randomNum >= 46 && randomNum <= 60) {
+    return console.log(`Random number ${randomNum} is in G. 46 - 60`);
 }
-
+else if (randomNum >= 61 && randomNum <= 75) {
+    return console.log(`Random number ${randomNum} is in O. 61 - 75`); 
+} 
+else 
+{
+return console.log(`Random number is invalid`);
+}
 return console.log(randomNum);
-
 }
+
+
+//switch
+//
+//    switch(randomNum){
+//        case randomNum == 1:
+//        console.log(`Random number is in B.`)
+//        break;
+//        case randomNum == 16:
+//        console.log(`Random number is in I`)
+//        break;
+//        case randomNum == 31:
+//        console.log(`Random number is in N`)
+//        break;
+//        case randomNum == 46:
+//        console.log(`Random number is in G`)
+//        break;
+//        case randomNum == 61:
+//        console.log(`Random number is in O`)
+//        break;
+//    }
+//    }
