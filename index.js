8fc49965fb1cc9d1120a8f24180d1020dc2e79e1
MarksIT23 <markangelo2304@gmail.com>
2024-03-19 15:25:40 +0800

    Updated Bingo Mechanics.

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-
 console.log("Hello World.");
 
-generateRandomNumber(30);
+let B = false, I = false, N = false, G = false, O = false;
 
+
+generateRandomNumber(30);
 function generateRandomNumber(max) 
+
 {
     let randomNum = Math.floor(Math.random() * max) + 1;
 
 if (randomNum <= 15){
-    return console.log(`Random number ${randomNum} is in B. 1 - 15`);
+    B = true
+    console.log(`Random number ${randomNum} is in B. 1 - 15`)
 } else if (randomNum >= 16 && randomNum <= 30) {
-return console.log(`Random number ${randomNum} is in I. 16 - 30`);
+    I = true
+ console.log(`Random number ${randomNum} is in I. 16 - 30`)
 } else if (randomNum >= 31 && randomNum <= 45) {
-    return console.log(`Random number ${randomNum} is in N. 31 - 45`);
+    N = true
+     console.log(`Random number ${randomNum} is in N. 31 - 45`)
 }  else if (randomNum >= 46 && randomNum <= 60) {
-    return console.log(`Random number ${randomNum} is in G. 46 - 60`);
+    G = true
+    console.log(`Random number ${randomNum} is in G. 46 - 60`)
 }
 else if (randomNum >= 61 && randomNum <= 75) {
-    return console.log(`Random number ${randomNum} is in O. 61 - 75`); 
+     O = true
+     console.log(`Random number ${randomNum} is in O. 61 - 75`);
 } 
-else 
+else {
+ console.log(`Random number is invalid`)} 
+
+ if(B == true && I == true && N == true && G == true && O == true)
 {
-return console.log(`Random number is invalid`);
+    console.log("BINGOOOOO!!!");
 }
-return console.log(randomNum);
-}
+
+} 
+
+
 
 
 //switch
